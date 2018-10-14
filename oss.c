31f217fdbdde89514096b48ed7ecfffe99399605
Jennifer Wasson <30751177+jlw2002us@users.noreply.github.com>
2018-10-13 19:47:12 -0500

    Add files via upload

--- oss.c
+++ oss.c
@@ -29,6 +29,7 @@
    long long int nanoseconds;
    int seconds;   
    int totalWaitTime;
+   int pidd;
    int HighStackQueue[20];
      int LowStackQueue[20];
   int j;
@@ -60,7 +61,7 @@
    write (STDOUT_FILENO,"Process terminated\n",16);
    shmdt (shmPTR);
    shmctl (shmid, IPC_RMID, 0);
-   sem_unlink ("pSem15");   
+   sem_unlink ("pSem2");   
     sem_close(sem);  
 
  exit(0);
@@ -83,6 +84,7 @@
      bool high = false;
      bool low = false;
      key_t shmkey;
+     pid_t pid;
 //     int HighStackQueue[20];
   //   int LowStackQueue[20];
      //int highStackNo = 0;
@@ -105,96 +107,97 @@
      
                                                  
      
-     sem = sem_open ("pSem15", O_CREAT | O_EXCL, 0644, 1); 
+     sem = sem_open ("pSem2", O_CREAT | O_EXCL, 0644, 1); 
       printf ("semaphores initialized.\n\n");
      sem_close(sem);
-     
-    while(childProcesses < 1){       
-        if (fork() == 0){
-                //get random number for priority
-             srand(i);
-             value =  (rand()%100);  
-             
-             //process IDs in queues- low or high
-             if (value <=  0){
-               shmPTR->processBlock.queueNo = 1;
-               shmPTR->HighStackQueue[shmPTR->j] = i; shmPTR->j++;
-               shmPTR->highStackNo++;
-              }
-             else{
-               shmPTR->processBlock.queueNo = 0;
-               shmPTR->LowStackQueue[shmPTR->k] = i;shmPTR->k++;
-               shmPTR->LowStackNo++;
-                }    
-                        
-                
-                execl("./user", (char*)i, NULL, NULL); 
-          }
-          
-         else{  sleep(2);
+     for(i=0; i < 2; i++){
+    
+        
+          if ((pid = fork()) == 0){
+               //char str[50];
+               //snprintf(str, sizeof(str), "%d", i);
+
+               shmPTR->pidd = i; //process ID
                
-           sem = sem_open("pSem15",0);
-           sem_wait(sem);
-            if(shmPTR->highStackNo != 0){high = true;
-             shmPTR->processID = shmPTR->HighStackQueue[shmPTR->j - shmPTR->highStackNo];}
-            else if(shmPTR->LowStackNo != 0){ 
-              low = true;
-              shmPTR->processID = shmPTR->LowStackQueue[shmPTR->k - shmPTR->LowStackNo];
+               
+               srand(i);
+               value =  (rand()%100);  
+             
+    //         process IDs in queues- low or high
+               if (value <  0){
+                 shmPTR->processBlock.queueNo = 1;
+                 shmPTR->HighStackQueue[shmPTR->j] = i; shmPTR->j++;
+                 shmPTR->highStackNo++;
+               }
+              else{
+                  shmPTR->processBlock.queueNo = 0;
+                  shmPTR->LowStackQueue[shmPTR->k] = i;shmPTR->k++;
+                  shmPTR->LowStackNo++;
+                  
+               }    
+                        
+                       char *args[]={"./user",NULL}; 
+                       execvp(args[0],args); 
+//                     execlp("./user", str, NULL, NULL); 
             }
-            sem_post(sem);
-            sem_close(sem);
+          
+          else{  sleep(2);
+               
+              sem = sem_open("pSem17",0);
+              sem_wait(sem);
+              if(shmPTR->highStackNo != 0){high = true;
+               shmPTR->processID = shmPTR->HighStackQueue[shmPTR->j - shmPTR->highStackNo];}
+              else if(shmPTR->LowStackNo != 0){ 
+                low = true; 
+                shmPTR->processID = shmPTR->LowStackQueue[shmPTR->k - shmPTR->LowStackNo];
+                fprintf(stderr,"%d",shmPTR->processID);
+              }
+             sem_post(sem);
+             sem_close(sem);
             sleep(2);
             if(shmPTR->terminated == 1){
               processesTerminated++;
-               if(high == true){
-                 shmPTR->highStackNo--; 
-                  high = false;}
-               if(low == true){
-                 shmPTR->LowStackNo--;
-                  low = false;}
-               
-               }
-            else{
-              
-              if(high == true){
-                shmPTR->HighStackQueue[shmPTR->j] = shmPTR->processID;
-                high = false; shmPTR->j++; }
-              if(low == true){ 
-                 shmPTR->LowStackQueue[shmPTR->k] = shmPTR->processID;
-                 low = false; shmPTR->k++;}
-
-            }}           
-           childProcesses++;i++;}
-         while(processesTerminated < 1){//fprintf(stderr,"%d",shmPTR->LowStackNo);
-            sem = sem_open("pSem15",0);
-            sem_wait(sem);
-            if(shmPTR->highStackNo != 0){high = true;
-               shmPTR->processID = shmPTR->HighStackQueue[shmPTR->j - shmPTR->highStackNo];}
-               else if(shmPTR->LowStackNo != 0){ low = true;
-                 shmPTR->processID = shmPTR->LowStackQueue[shmPTR->k - shmPTR->LowStackNo];
-                // fprintf(stderr,"%d",shmPTR->processID);
-                }
-              sem_post(sem);
-              sem_close(sem);
-              
-              sleep(2);
-             
-             if(shmPTR->terminated == 1){  //fprintf(stderr,"%d",shmPTR->processID);
                if(high == true){
                  shmPTR->highStackNo--; 
                  high = false;}
                if(low == true){
                  shmPTR->LowStackNo--;
                  low = false;}
-               processesTerminated++;}
-              else{
-                 if(high == true){
-                   shmPTR->HighStackQueue[shmPTR->j] = shmPTR->processID;
-                    high = false; shmPTR->j++;}
-               if(low == true){
-                 shmPTR->LowStackQueue[shmPTR->k] = shmPTR->processID;
-                 low = false; shmPTR->k++;}}
-         }
+               
+               }}}
+            
+         //if(pid != 0){
+           //printf("hi");         
+           //sem = sem_open("pSem15",0);
+           //sem_wait(sem);
+           //if(shmPTR->highStackNo != 0){high = true;
+             // shmPTR->processID = shmPTR->HighStackQueue[shmPTR->j - shmPTR->highStackNo];}
+              //else if(shmPTR->LowStackNo != 0){ low = true;
+                //shmPTR->processID = shmPTR->LowStackQueue[shmPTR->k - shmPTR->LowStackNo];
+                // fprintf(stderr,"%d",shmPTR->processID);
+               // }
+              //sem_post(sem);
+              //sem_close(sem);
+              
+              //sleep(2);
+             
+              //if(shmPTR->terminated == 1){  //fprintf(stderr,"%d",shmPTR->processID);
+                //if(high == true){
+                  //shmPTR->highStackNo--; 
+                  //high = false;}
+                //if(low == true){
+                  //shmPTR->LowStackNo--;
+                  //low = false;}
+                //processesTerminated++;}
+              // else{
+                // if(high == true){
+                  //shmPTR->HighStackQueue[shmPTR->j] = shmPTR->processID;
+                  //high = false; shmPTR->j++;}
+                 //if(low == true){
+                   //shmPTR->LowStackQueue[shmPTR->k] = shmPTR->processID;
+                   //low = false; shmPTR->k++;}}
+          // }  
+         //}
          sleep(2);
          killpg(getpgid(getpid()), SIGTERM);
        
--- user.c
+++ user.c
@@ -11,69 +11,79 @@
  #include <wait.h>
 
 
- struct ProcessBlock{
-  int queueNo;
-   
-   int ready;
- };
+struct ProcessBlock{
+    int queueNo; //1 - high, 0- low
+  
+  
+  int ready; //if process is ready to be run = 1
+    };
 
  struct Memory{
    long long int nanoseconds;
-   int seconds;
+   int seconds;   
    int totalWaitTime;
-   int processID;
+   int pidd;
    int HighStackQueue[20];
-   int highStackNo;
    int LowStackQueue[20];
+   int j;
+   int k;
+   int highStackNo;
    int LowStackNo;
-   int j; int k;
+   int processID;
    long long int totalCPU;
    int terminated; //tell parent that it's done
-   struct ProcessBlock processBlock;   
- };
- struct Memory *shmPTR;
-  sem_t* sem;
+   struct ProcessBlock processBlock;
+    };
+
+   struct Memory *shmPTR;
+   sem_t* sem;
 
 
 
  int main(int argc, char* argv[]){
-       
+    printf("hello from exec");       
     key_t  ShmKEY;
     int ShmID;
     int terminated = 0;
     int value = 0;
     int flag = 0;
     long int getrand = getpid();
-    int processID = atoi(argv[1]);
+    int processID = 0;    
     int quantum = 2000;
     long long int CPUused = 0;     
     long long int x;
     long long int arrival_nanoseconds = 0;
     int arrival_seconds = 0;
    
-   
+  // printf("%d",processID);   
    ShmKEY = ftok(".",'x');
    ShmID = shmget(ShmKEY, sizeof(struct Memory), 0666);
    if (ShmID < 0){
      printf("*** shmget error (client) ***\n");
-    exit(1);
+     exit(1);
     }
     shmPTR = (struct Memory *) shmat(ShmID, NULL, 0);
    if(ShmID == -1){
      printf("*** shmat error(client) ***\n");
      exit(1);
     }
-   
+//      sem = sem_open("p17", 0);
+  //    sem_wait(sem);
+      processID = shmPTR->pidd;
+//      fprintf(stderr,"%d", processID);
+    //  sem_post(sem); sem_close(sem);
+  //  printf("%d",processID);
+
    if( shmPTR->processBlock.queueNo == 1)
      quantum = 1000;    
-while(terminated == 0){               
-    while(x < 10000000){
+//while(terminated == 0){               
+  while(x < 10000000){
                     
-      sem = sem_open("pSem15",0);
-      sem_wait(sem);
+    sem = sem_open("pSem2",0);
+    sem_wait(sem);
       
-      if(processID == shmPTR->processID)
-        { printf("Process %d  is dispatched ...\n", processID);
+    if(processID == shmPTR->processID)
+       { printf("Process %d  is dispatched ...\n", processID);
             srand(getrand++);
             value =  (rand()%100);
             printf("%d", value);
@@ -86,58 +96,58 @@ while(terminated == 0){
             if(value >70)
               flag = 3; //get preempted before finishing quantum
             break;}                                    
-         sem_post(sem);                                                                  
-         sem_close(sem);
+          sem_post(sem);                                                                  
+          sem_close(sem);
 //        printf("%d",flag);
   
           x++; }
           
-    switch(flag){
-      case 0: 
-         shmPTR->terminated = 1;
-         terminated = 1;
-         printf("Process %d terminated .. \n", processID);
-         break;
-      case 1:
-         shmPTR->nanoseconds = shmPTR->nanoseconds + quantum;
-         shmPTR->terminated = 1;
-         terminated = 1;
-         printf("Process %d ran for %d nanoseconds and terminated.. \n",processID,quantum);
-         shmPTR->totalCPU = shmPTR->totalCPU + quantum;
-         break;
-      case 2:
-          srand(getrand++);
-          int r = (rand()%5);
-          srand(getrand++);
-          int s = (rand()%1000);
-          shmPTR->seconds = shmPTR->seconds + r;
-          shmPTR->nanoseconds = shmPTR->nanoseconds + s;
-          printf("Process %d waited for event for %d seconds, %d nanoseconds..\n", processID, r, s);
-          shmPTR->terminated = 0;
+    //switch(flag){
+      //case 0: 
+        // shmPTR->terminated = 1;
+         //terminated = 1;
+         //printf("Process %d terminated .. \n", processID);
+         //break;
+      //case 1:
+        // shmPTR->nanoseconds = shmPTR->nanoseconds + quantum;
+         //shmPTR->terminated = 1;
+         //terminated = 1;
+         //printf("Process %d ran for %d nanoseconds and terminated.. \n",processID,quantum);
+         //shmPTR->totalCPU = shmPTR->totalCPU + quantum;
+         //break;
+      //case 2:
+        //  srand(getrand++);
+          //int r = (rand()%5);
+          //srand(getrand++);
+          //int s = (rand()%1000);
+          //shmPTR->seconds = shmPTR->seconds + r;
+          //shmPTR->nanoseconds = shmPTR->nanoseconds + s;
+          //printf("Process %d waited for event for %d seconds, %d nanoseconds..\n", processID, r, s);
+          //shmPTR->terminated = 0;
           
-          terminated = 0;
-          break;
-      case 3:
-          srand(getrand++);
-          int p = 1 + (rand()%99);
-          float percentage = (float)p/100;
-          shmPTR->nanoseconds = shmPTR->nanoseconds + (long long int)(percentage*quantum);
-          shmPTR->totalCPU = shmPTR->totalCPU + (long long int)percentage*quantum;
-          printf("Process %d ran for %lld nanoseconds ..\n", processID, (long long int)(percentage*quantum));
-          shmPTR->terminated = 0;
+          //terminated = 0;
+          //break;
+      //case 3:
+        //  srand(getrand++);
+          //int p = 1 + (rand()%99);
+          //float percentage = (float)p/100;
+          //shmPTR->nanoseconds = shmPTR->nanoseconds + (long long int)(percentage*quantum);
+          //shmPTR->totalCPU = shmPTR->totalCPU + (long long int)percentage*quantum;
+          //printf("Process %d ran for %lld nanoseconds ..\n", processID, (long long int)(percentage*quantum));
+          //shmPTR->terminated = 0;
           
-          terminated = 0;
-          break;
-      default:
-         printf("Error");
-         break;          
-     }
-    sem_post(sem);
-    sem_close(sem);
-    x = 0;
-     if(shmPTR->terminated == 1)
-       break;
-     }
+          //terminated = 0;
+          //break;
+      //default:
+        // printf("Error");
+         //break;          
+     //}
+    //sem_post(sem);
+    //sem_close(sem);
+    //x = 0;
+     //if(shmPTR->terminated == 1)
+       //break;
+     //}
  
          shmdt((void *) shmPTR);
    
